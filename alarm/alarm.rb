f974f04e82905f7ff8441d21a21677f4a4f7e868
Ashley Hedberg <ashley.hedberg@tufts.edu>
2013-10-03 01:11:51 -0400

    nmap scan detection completed

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -1,8 +1,63 @@
 require 'packetfu'
 
-stream = PacketFu::Capture.new(:start => true, :iface => 'eth0', 
+def write_alert(incident_number, attack, source_ip, protocol)
+  puts "#{incident_number}. ALERT: #{attack} is detected from #{source_ip} (#{protocol})!"
+end
+
+stream = PacketFu::Capture.new(:start => true, :iface => 'lo', \
                                :promisc => true)
+alert_num = 0
 
 stream.stream.each do |raw|
-  # ...
+  packet = PacketFu::Packet.parse(packet = raw)
+  
+  payload = packet.eth_header.body  # in string form when not encrypted
+
+  begin
+    # Check for nmap scans - TCP packets
+    flags = packet.tcp_flags
+
+    if (!(flags.urg && flags.ack && flags.psh && flags.rst && flags.syn && \
+          flags.fin))
+      # Found a NULL scan
+      alert_num += 1
+      write_alert(alert_num, "NULL scan", packet.ip_saddr, packet.proto()[-1])
+    else if ((flags.urg == 1) && (flags.psh == 1) && (flags.fin == 1))
+      # Found an Xmas scan
+      alert_num += 1
+      write_alert(alert_num, "Xmas scan", packet.ip_saddr, packet.proto()[-1])
+    end
+  end
+  rescue NoMethodError
+  end
+
+  # TODO Check for other nmap scans
+  if (payload.include? "nmap")
+    alert_num += 1
+    write_alert(alert_num, "Nmap scan", packet.ip_saddr, packet.proto()[-1])
+  end
+  if (payload.include? "NMAP")
+    alert_num += 1
+    write_alert(alert_num, "Nmap scan", packet.ip_saddr, packet.proto()[-1])
+  end
+  if (payload.include? "Nmap")
+    alert_num += 1
+    write_alert(alert_num, "Nmap scan", packet.ip_saddr, packet.proto()[-1])
+  end
+
+  # TODO Check for in-the-clear password leakage
+  if (payload.include? "PASS")
+    alert_num += 1
+    write_alert(alert_num, "Password leakage", packet.ip_saddr, packet.proto()[-1])
+  end
+
+  # TODO Check for in-the-clear credit card number leakage
+
+
+  # TODO Check for cross-site scripting
+  if (payload.include? "<script>")
+    alert_num += 1
+    write_alert(alert_num, "XSS", packet.ip_saddr, packet.proto()[-1])
+  end
 end
+
