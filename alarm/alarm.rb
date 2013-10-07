dafa4def3c0b640c1cbce57155b4af9c652e22d1
Ashley Hedberg <ashley.hedberg@tufts.edu>
2013-10-07 00:57:00 -0400

    finished writing alert cases, needs testing

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -4,8 +4,7 @@ def write_alert(incident_number, attack, source_ip, protocol)
   puts "#{incident_number}. ALERT: #{attack} is detected from #{source_ip} (#{protocol})!"
 end
 
-stream = PacketFu::Capture.new(:start => true, :iface => 'lo', \
-                               :promisc => true)
+stream = PacketFu::Capture.new(:start => true, :iface => 'lo', :promisc => true)
 alert_num = 0
 
 stream.stream.each do |raw|
@@ -14,47 +13,53 @@ stream.stream.each do |raw|
   payload = packet.eth_header.body  # in string form when not encrypted
 
   begin
-    # Check for nmap scans - TCP packets
+    # Check for nmap scans - Non-TCP packets will throw errors here
     flags = packet.tcp_flags
 
-    if (!(flags.urg && flags.ack && flags.psh && flags.rst && flags.syn && \
-          flags.fin))
+    if (!(flags.urg || flags.ack || flags.psh || flags.rst || flags.syn || flags.fin))
       # Found a NULL scan
       alert_num += 1
       write_alert(alert_num, "NULL scan", packet.ip_saddr, packet.proto()[-1])
+
     else if ((flags.urg == 1) && (flags.psh == 1) && (flags.fin == 1))
       # Found an Xmas scan
       alert_num += 1
       write_alert(alert_num, "Xmas scan", packet.ip_saddr, packet.proto()[-1])
+
+    end
+    
+    # Check for other nmap scans
+    if (payload.include? "nmap")
+      alert_num += 1
+      write_alert(alert_num, "Nmap scan", packet.ip_saddr, packet.proto()[-1])
+    end
+    if (payload.include? "NMAP")
+      alert_num += 1
+      write_alert(alert_num, "Nmap scan", packet.ip_saddr, packet.proto()[-1])
+    end
+    if (payload.include? "Nmap")
+      alert_num += 1
+      write_alert(alert_num, "Nmap scan", packet.ip_saddr, packet.proto()[-1])
     end
   end
+
   rescue NoMethodError
   end
 
-  # TODO Check for other nmap scans
-  if (payload.include? "nmap")
-    alert_num += 1
-    write_alert(alert_num, "Nmap scan", packet.ip_saddr, packet.proto()[-1])
-  end
-  if (payload.include? "NMAP")
-    alert_num += 1
-    write_alert(alert_num, "Nmap scan", packet.ip_saddr, packet.proto()[-1])
-  end
-  if (payload.include? "Nmap")
-    alert_num += 1
-    write_alert(alert_num, "Nmap scan", packet.ip_saddr, packet.proto()[-1])
-  end
 
-  # TODO Check for in-the-clear password leakage
+  # Check for in-the-clear password leakage
   if (payload.include? "PASS")
     alert_num += 1
     write_alert(alert_num, "Password leakage", packet.ip_saddr, packet.proto()[-1])
   end
 
-  # TODO Check for in-the-clear credit card number leakage
+  # Check for in-the-clear credit card number leakage
+  if (/\d{4}(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/.match(payload))
+    alert_num += 1
+    write_alert(alert_num, "Credit card number in the clear", packet.ip_saddr, packet.proto()[-1])
+  end
 
-
-  # TODO Check for cross-site scripting
+  # Check for cross-site scripting
   if (payload.include? "<script>")
     alert_num += 1
     write_alert(alert_num, "XSS", packet.ip_saddr, packet.proto()[-1])
